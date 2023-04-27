a78b82003fbd527f0810c71227b65f7cc5216667
warlockja <warlockja@gmail.com>
2023-04-27 13:35:49 +0400

    cleanup

--- src/routes/userdata.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const userdataRouter = express.Router();
-import userdataController from '../controllers/userdataController.js';
-
-userdataRouter.route('/')
-    .get(userdataController.getGeodata);
-
-export default userdataRouter;
