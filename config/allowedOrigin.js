c284514595f5d2eb424765ec2a3f9b0f3de17d49
warlockja <warlockja@gmail.com>
2023-06-15 19:03:13 +0300

    cors 3

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,3 +1,3 @@
-const allowedOrigin = ["https://daily-planner-sigma.vercel.app/"];
+const allowedOrigin = ["https://daily-planner-sigma.vercel.app"];
 export default allowedOrigin;
 //# sourceMappingURL=allowedOrigin.js.map
