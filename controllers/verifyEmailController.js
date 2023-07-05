b2cda09deba5adbf080e79320ec7a9b6b5b8793d
warlockja <warlockja@gmail.com>
2023-07-05 14:20:56 +0300

    testing redirect on email verification

--- controllers/verifyEmailController.js
+++ controllers/verifyEmailController.js
@@ -1,41 +1,57 @@
-import { pool } from '../db/DBConnect.js';
-import compareTimestamps from '../util/compareTimestamps.js';
-import Joi from 'joi';
+import { pool } from "../db/DBConnect.js";
+import compareTimestamps from "../util/compareTimestamps.js";
+import Joi from "joi";
+import { config } from "dotenv";
+config();
+
 const schemaVerifyUser = Joi.object({
-    email: Joi.string().email().required(),
-    token: Joi.string().required()
+  email: Joi.string().email().required(),
+  token: Joi.string().required(),
 });
+
 const verifyUser = async (req, res) => {
-    // Joi validation
-    const validationResult = await schemaVerifyUser.validate(req.params);
-    if (validationResult.error)
-        return res.status(400).json(validationResult.error.details[0].message);
-    // valid fields
-    const { email, token } = validationResult.value;
-    try {
-        // getting email verification data stored in DB
-        const result = await pool.execute('SELECT * FROM verify WHERE email = ?', [email]);
-        // checking that verification request was issued
-        // TODO confirm that will work with multiple outdated requests present
-        if (Array.isArray(result[0]) && result[0].length === 0)
-            return res.sendStatus(400);
-        const verificationRequest = result[0][0];
-        // verifying token data
-        if (verificationRequest.token !== token)
-            return res.status(401).json({ message: 'Incorrect verification data' });
-        // checking that verification request is not outdated
-        // compareTimestamps accepts MySQL timestamp string, MySQL Offset (Planetscale MySQL UTC-4), and max delta in seconds
-        if (!compareTimestamps({ mySQLTimestamp: verificationRequest.ts, mySQLOffset: -4, maxDelta: 10 * 60 }))
-            return res.status(400).json({ message: 'Verification link expired' });
-        // applying verified status and removing verification request data from the DB
-        await pool.execute('DELETE FROM verify WHERE email = ?', [email]);
-        await pool.execute('UPDATE users SET email_confirmed = ? WHERE email = ?', [true, email]);
-        // TODO return a proper html response
-        return res.status(200).json({ message: `${email} verified` });
-    }
-    catch (error) {
-        return res.status(500).json({ message: error.stack });
-    }
+  // Joi validation
+  const validationResult = await schemaVerifyUser.validate(req.params);
+  if (validationResult.error)
+    return res.status(400).json(validationResult.error.details[0].message);
+  // valid fields
+  const { email, token } = validationResult.value;
+  try {
+    // getting email verification data stored in DB
+    const result = await pool.execute("SELECT * FROM verify WHERE email = ?", [
+      email,
+    ]);
+    // checking that verification request was issued
+    // TODO confirm that will work with multiple outdated requests present
+    if (Array.isArray(result[0]) && result[0].length === 0)
+      return res.sendStatus(400);
+    const verificationRequest = result[0][0];
+    // verifying token data
+    if (verificationRequest.token !== token)
+      return res.status(401).json({ message: "Incorrect verification data" });
+    // checking that verification request is not outdated
+    // compareTimestamps accepts MySQL timestamp string, MySQL Offset (Planetscale MySQL UTC-4), and max delta in seconds
+    if (
+      !compareTimestamps({
+        mySQLTimestamp: verificationRequest.ts,
+        mySQLOffset: -4,
+        maxDelta: 10 * 60,
+      })
+    )
+      return res.status(400).json({ message: "Verification link expired" });
+    // applying verified status and removing verification request data from the DB
+    await pool.execute("DELETE FROM verify WHERE email = ?", [email]);
+    await pool.execute("UPDATE users SET email_confirmed = ? WHERE email = ?", [
+      true,
+      email,
+    ]);
+    // TODO return a proper html response
+    // return res.status(200).json({ message: `${email} verified` });
+    res.status(200).redirect(process.env.ALLOWED_ORIGIN_PROD);
+    return res.status(200).json({ message: `${email} verified` });
+  } catch (error) {
+    return res.status(500).json({ message: error.stack });
+  }
 };
 export default { verifyUser };
 //# sourceMappingURL=verifyEmailController.js.map
