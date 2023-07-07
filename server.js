98fcd668a216d78931a1c26cadfcae4e1f392209
warlockja <warlockja@gmail.com>
2023-07-07 17:41:51 +0300

    testing

--- server.js
+++ server.js
@@ -21,15 +21,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static("public"));
 app.set("views", "./public/views");
 app.set("view engine", "pug");
 
 // routes unaffected by cors
 // app.get("/", (req, res) => res.send("API is running"));
-app.get("^/$|/index(.html)?", (req, res) =>
-  res.render("index", {
-    title: "Daily Planner API",
-  })
+app.get(
+  "^/$|/index(.html)?",
+  (req, res) =>
+    res.sendFile("index.html", { root: path.join(__dirname, "public") })
+  // res.render("index", {
+  //   title: "Daily Planner API",
+  // })
 ); // echo testing route
 app.use("/verify", verifyEmailRouter); // route to accept email confirmation requests from a link in email
 app.use("/reset", resetPassword); // route to accept password reset requests from a link in email
