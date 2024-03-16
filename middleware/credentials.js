8df322a88dba1d456e412b85167bc9a5e3839d66
warlockja <warlockja@gmail.com>
2024-03-16 15:15:27 +0300

    CORS fix

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,6 +3,7 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigin.includes(origin)) {
     res.header({ "Access-Control-Allow-Credentials": true });
+    res.header({ "Access-Control-Allow-Origin": origin });
   }
   next();
 };
