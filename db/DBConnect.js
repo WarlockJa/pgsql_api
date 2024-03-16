5a33859cca5d01bd015e53fbf8a3debd9e9bb972
warlockja <warlockja@gmail.com>
2024-03-16 17:01:20 +0300

    fix DB connect

--- db/DBConnect.js
+++ db/DBConnect.js
@@ -3,35 +3,37 @@
 import { config } from "dotenv";
 import mysql from "mysql2/promise";
 config();
+
 export const pool = mysql.createPool({
-    host: process.env.MSQL_HOST,
-    user: process.env.MSQL_USER,
-    database: process.env.MSQL_DB,
-    password: process.env.MSQL_PSWD,
-    ssl: {
-        rejectUnauthorized: true,
-    },
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.MSQL_HOST,
+  user: process.env.MSQL_USER,
+  database: process.env.MSQL_DB,
+  password: process.env.MSQL_PSWD,
+  port: process.env.MSQL_PORT,
+  ssl: {
+    rejectUnauthorized: true,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 export const ACCEPTED_LOCALES = [
-    "en",
-    "ru",
-    "ru-RU",
-    "en-US",
-    "en-GB",
-    "en-ZW",
-    "en-AU",
-    "en-BZ",
-    "en-CA",
-    "en-IE",
-    "en-JM",
-    "en-NZ",
-    "en-PH",
-    "en-ZA",
-    "en-TT",
-    "en-VI",
+  "en",
+  "ru",
+  "ru-RU",
+  "en-US",
+  "en-GB",
+  "en-ZW",
+  "en-AU",
+  "en-BZ",
+  "en-CA",
+  "en-IE",
+  "en-JM",
+  "en-NZ",
+  "en-PH",
+  "en-ZA",
+  "en-TT",
+  "en-VI",
 ];
 // export default {
 //     query: (text: string, params?: any, callback?: (err: Error, result: pg.QueryResult<any>) => void) => {
