c2d13f04569f64b8e401470e69d10dc859022c80
warlockja <warlockja@gmail.com>
2024-03-16 17:13:40 +0300

    fix CORS

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,14 +2,10 @@ import allowedOrigin from "./allowedOrigin.js";
 // corsOptions from the documentation
 const corsOptions = {
   origin: (origin, callback) => {
-    // TEST
-    // callback(null, origin);
     if (allowedOrigin.indexOf(origin) !== -1) {
-      console.log("PASSED: ", allowedOrigin);
       // !origin for localhost testing
       callback(null, origin);
     } else {
-      console.log("FAILED: ", allowedOrigin);
       callback(new Error("Not allowed by CORS"));
     }
   },
--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -4,6 +4,7 @@ config();
 const allowedOrigin = [
   process.env.ALLOWED_ORIGIN_PROD,
   process.env.ALLOWED_ORIGIN_DEV,
+  process.env.BASE_URI,
 ];
 
 export default allowedOrigin;
